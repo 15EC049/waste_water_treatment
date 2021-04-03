4ef0d911f80891da0f309f1d61facd0b53373afd
Umar B <umarbasheer1996@gmail.com>
2021-04-03 14:36:58 +0530

    Update ard_py_comm.py

--- Python/ard_py_comm.py
+++ Python/ard_py_comm.py
@@ -1,17 +1,48 @@
+import blynklib
 import serial
-import time
 
-arduino = serial.Serial(port='COM4', baudrate=115200, timeout=.1)
+ser = serial.Serial('/dev/ttyUSB1')
+BLYNK_AUTH = 'dcNjKdO7nfeIlRyZ8lMpceB6IJ5om5x7'
 
+# initialize Blynk
+blynk = blynklib.Blynk(BLYNK_AUTH)
 
-def write_read(x):
-    arduino.write(bytes(x, 'utf-8'))
-    time.sleep(0.05)
-    data = arduino.readline()
-    return data
+WRITE_EVENT_PRINT_MSG = "[WRITE_VIRTUAL_PIN_EVENT] Pin: V{} Value: '{}'"
 
 
+# register handler for virtual pin V4 write event
+@blynk.handle_event('write V0')
+def write_virtual_pin_handler(pin, value):
+    #print(WRITE_EVENT_PRINT_MSG.format(pin, value))
+    print('V0 Value')
+    if ( str(value)==str("['1']")):
+    	print (pin)
+    	ser.write(b'1')
+
+
+
+@blynk.handle_event('write V1')
+def write_virtual_pin_handler(pin, value):
+    #print(WRITE_EVENT_PRINT_MSG.format(pin, value))
+    print('V1 Value')
+    if ( str(value)==str("['1']")):
+    	print (pin)
+    	ser.write(b'2')
+
+@blynk.handle_event('write V2')
+def write_virtual_pin_handler(pin, value):
+    #print(WRITE_EVENT_PRINT_MSG.format(pin, value))
+    print('V2 Value')
+    if ( str(value)==str("['1']")):
+    	print (pin)
+    	ser.write(b'3')
+
+###########################################################
+# infinite loop that waits for event
+###########################################################
 while True:
-    num = input("Enter a number: ")
-    value = write_read(num)
-    print(value)
+    #print(ser.name) 
+    blynk.run()
+    #ser.write(b'hello')
+    
+ser.close()
